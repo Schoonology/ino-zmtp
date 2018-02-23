c7ec5a6fa601ebefbd1b7f52b6008afe89f1f21b
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2018-02-23 15:29:21 -0600

    Refactor: Clean up local test helper.

--- Makefile
+++ Makefile
@@ -1,15 +1,15 @@
 .PHONY: format test watch
 
-test.bin: src/* test/*
+test.bin: src/* test/*.ino
 	particle compile photon . --saveTo test.bin
 
 local-helper: test/local-helper.cpp
-	clang test/local-helper.cpp -o local-helper -lczmq
+	clang -fno-exceptions test/local-helper.cpp -o local-helper -lczmq
 
 local-tester: test/local-tester.cpp
 	clang test/local-tester.cpp -o local-tester -lczmq
 
-test: test.bin local-helper
+test: local-helper test.bin
 	particle flash --usb test.bin
 	./local-helper
 
--- test/local-helper.cpp
+++ test/local-helper.cpp
@@ -3,40 +3,45 @@
 #include "assert.h"
 #include "czmq.h"
 
-/**
- * Run a peer through the test scenario.
- *
- * NOTE: We're playing fast and loose with memory here; our assumption is
- * that this will be run from an OS that will reap the memory after the
- * process exits, so we're not freeing the memory manually. This may be
- * fixed in a future refactor.
- */
-int main(int argc, char const *argv[]) {
-  zsock_t *router = zsock_new_router("tcp://0.0.0.0:1234");
+class SocketGuard {
+public:
+  SocketGuard(zsock_t *socket) : socket(socket) {}
+  ~SocketGuard() { zsock_destroy(&socket); }
+  zsock_t *socket;
+};
+
+const uint8_t *runDealerTest(const char *endpoint) {
+  printf("\n");
+  printf("DEALER tests:\n");
+  printf("=============\n");
+
+  zsock_t *router = zsock_new_router(endpoint);
   assert(router);
+  SocketGuard _(router);
 
   //
   // Receive connection from peer's DEALER
   //
   printf("Client should connect and send a greeting: ");
   zmsg_t *greeting = zmsg_recv(router);
-  zmsg_t *duplicate = zmsg_dup(greeting);
   printf("✔\n");
 
   //
   // Identity frame
   //
-  printf("Client should provide an Identity: ");
+  printf("Client should respond with a single frame: ");
   size_t msg_size = zmsg_size(greeting);
   if (msg_size != 2) {
     printf("✘ — Invalid number of frames (%lu)\n", msg_size);
-    return 1;
+    return NULL;
   }
+  printf("✔\n");
 
-  char *uuid = zmsg_popstr(greeting);
-  if (!streq(uuid, "Photon")) {
-    printf("✘ — Invalid Identity (%s)\n", uuid);
-    return 1;
+  printf("Client should provide an Identity: ");
+  char *identity = zmsg_popstr(greeting);
+  if (!streq(identity, "Photon")) {
+    printf("✘ — Invalid Identity (%s)\n", identity);
+    return NULL;
   }
   printf("✔\n");
 
@@ -47,7 +52,7 @@ int main(int argc, char const *argv[]) {
   char *message = zmsg_popstr(greeting);
   if (!streq(message, "Hello")) {
     printf("✘ — Invalid message (%s)\n", message);
-    return 1;
+    return NULL;
   }
   printf("✔\n");
 
@@ -61,20 +66,23 @@ int main(int argc, char const *argv[]) {
   size_t address_size = zframe_size(frame);
   if (address_size != 4) {
     printf("✘ — Invalid address size (%lu)\n", address_size);
-    return 1;
+    return NULL;
   }
-  uint8_t *bytes = zframe_data(frame);
-  char address_string[50];
-  sprintf(address_string, "tcp://%i.%i.%i.%i:1235", bytes[0], bytes[1],
-          bytes[2], bytes[3]);
-  printf("Address: %s\n", address_string);
   printf("✔\n");
+  return zframe_data(frame);
+}
+
+bool runRouterTest(const char *endpoint) {
+  printf("\n");
+  printf("ROUTER tests:\n");
+  printf("=============\n");
 
   //
   // Connect to peer's ROUTER at the specified address
   //
-  zsock_t *dealer = zsock_new_dealer(address_string);
+  zsock_t *dealer = zsock_new_dealer(endpoint);
   assert(dealer);
+  SocketGuard _(dealer);
 
   //
   // Send a request
@@ -92,23 +100,46 @@ int main(int argc, char const *argv[]) {
   size_t answer_size = zmsg_size(answer);
   if (answer_size != 1) {
     printf("✘ — Invalid number of frames (%lu)\n", answer_size);
-    return 1;
+    return false;
   }
+  printf("✔\n");
 
   printf("Client should respond with a 2-octet answer: ");
   char *answer_str = zmsg_popstr(answer);
   if (!streq(answer_str, "42")) {
     printf("✘ — Invalid answer (%s)\n", answer_str);
-    return 1;
+    return false;
   }
   printf("✔\n");
 
-  //
-  // Cleanup
-  //
-  printf("Tearing down.\n");
-  zsock_destroy(&router);
-  zsock_destroy(&dealer);
+  return true;
+}
+
+/**
+ * Run a peer through the test scenario.
+ *
+ * NOTE: We're playing fast and loose with memory here; our assumption is
+ * that this will be run from an OS that will reap the memory after the
+ * process exits, so we're not freeing the memory manually. This may be
+ * fixed in a future refactor.
+ */
+int main(int argc, char const *argv[]) {
+  const uint8_t *peerAddress = runDealerTest("tcp://0.0.0.0:1234");
+  if (!peerAddress) {
+    return 1;
+  }
+
+  char peerEndpoint[50];
+  sprintf(peerEndpoint, "tcp://%i.%i.%i.%i:1235", peerAddress[0],
+          peerAddress[1], peerAddress[2], peerAddress[3]);
+
+  printf("\nPeer endpoint: %s\n", peerEndpoint);
+
+  if (!runRouterTest(peerEndpoint)) {
+    return 1;
+  }
+
+  printf("\nTearing down.\n");
   return 0;
 }
 
